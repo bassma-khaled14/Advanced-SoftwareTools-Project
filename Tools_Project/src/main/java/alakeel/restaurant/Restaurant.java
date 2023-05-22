d222412e4b4a2f4bb72a976adbe58a1099864099
Shrouk Ashraf <ashrafshrouk195@gmail.com>
2023-05-22 14:08:14 +0300

    added entity manager object

--- Tools_Project/src/main/java/alakeel/restaurant/Restaurant.java
+++ Tools_Project/src/main/java/alakeel/restaurant/Restaurant.java
@@ -4,29 +4,33 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Query;
 import javax.validation.constraints.NotNull;
 @Entity
-public class Restaurant implements Serializable
-{
+public class Restaurant implements Serializable {
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int resid;
 	@NotNull
 	private String name;
-	private int ownerid;                       //foriegnkey
+	private int ownerid;                       
 	@OneToMany(mappedBy = "meal")
     private List<Meal> mealList;
     @NotNull
     private String address;
-    public Restaurant(int resid, String name, int ownerid, List<Meal> mealList) {
+    EntityManager entityManager;
+    public Restaurant(int resid, String name, int ownerid, List<Meal> mealList, String address) {
         this.resid = resid;
         this.name = name;
         this.ownerid = ownerid;
         this.mealList = mealList;
+        this.address=address;
     }
     public Restaurant() {}
 
@@ -55,14 +59,25 @@ public class Restaurant implements Serializable
     public void setownerid(int ownerid) {
         this.ownerid = ownerid;
     }
-
     public List<Meal> getMealList() {
+    	Query query=entityManager.createQuery(address);
+    	List<Meal>MealList=query.getResultList();
+    			
         return mealList;
     }
 
     public void setMealList(List<Meal> mealList) {
         this.mealList = mealList;
     }
+    public void setaddress(String address)
+    {
+    	this.address=address;
+    }
+    public String getadress()
+    {
+    	return address;
+    }
+	
 }
     
 
