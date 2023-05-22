f493b758135ed6608390232d01b44030d0bc956f
Shrouk Ashraf <ashrafshrouk195@gmail.com>
2023-05-22 06:17:38 +0300

    fix : setDelivered

--- Tools_Project/src/main/java/alakeel/customer/Order.java
+++ Tools_Project/src/main/java/alakeel/customer/Order.java
@@ -1,5 +1,8 @@
 package alakeel.customer;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,21 +11,88 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
+import alakeel.restaurant.Restaurant;
 import alakeel.runner.Runner;
 
 @Entity
-public class Order {
+public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
     private int orderid;
+	@ManyToOne
 	@JoinColumn(name="runnerid")
 	private Runner runner;
-
 	@NotNull
 	private boolean delivered;
-	public void setDelivered(boolean delivered) {
-		this.delivered=delivered;
-		// TODO Auto-generated method stub
-		
-	}
+	private List<String> itemarr;
+    private double totalPrice;
+    private String orderStatus;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    // constructors
+    public Order() {}
+
+    public Order(List<String> itemarr, double totalPrice, Runner runner, Restaurant restaurant) {
+        this.itemarr = itemarr;
+        this.totalPrice = totalPrice;
+        this.runner = runner;
+        this.restaurant = restaurant;
+        this.orderStatus = "preparing";
+    }
+
+    // getters and setters
+    public int getorderid() {
+        return orderid;
+    }
+
+    public void setorderid(int orderid) {
+        this.orderid = orderid;
+    }
+
+    public List<String> getitemarr() {
+        return itemarr;
+    }
+
+    public void setitemarr(List<String> itemarr) {
+        this.itemarr = itemarr;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+    
+        this.orderStatus = orderStatus;
+    }    
+    public Runner getRunner() {
+        return runner;
+    }
+
+    public void setRunner(Runner runner) {
+        this.runner = runner;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public void setDelivered(boolean delivered) {
+		this.delivered=delivered;			
+		}	
+
 }
